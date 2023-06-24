c24c291f539180f19aac60571c97dc989289917f
Worlanyo <worladamalie@gmail.com>
2023-06-24 22:09:08 +0000

    Fix on mobile for modal close button

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -33,7 +33,7 @@ const Modal = ({ isOpen, onClose, children }: {isOpen: boolean , onClose: () => 
             aria-modal="true"
             aria-labelledby="modal-title"
           >
-            <div className="flex justify-end p-4">
+            <div className="flex justify-end p-4 mt-8 md:mt-0">
               <button
                 className="text-gray-500 hover:text-gray-700 focus:outline-none"
                 onClick={onClose}
