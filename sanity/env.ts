96b99b0e8049a3e6539b9c7cf9d8cdc4ed576d94
Worlanyo <worladamalie@gmail.com>
2023-07-27 17:59:49 +0000

    assert env variables

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,13 +1,21 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-06-02'
 
+// export const dataset = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_DATASET,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+// )
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "production",
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
+// export const projectId = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+// )
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "s9y7etk0",
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
